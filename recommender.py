3a9957649344416d45fc019fe7898943a1efc96e
Lina Hu <wehu12@gmail.com>
2014-12-01 23:27:22 -0500

    aaa

--- recommender.py
+++ recommender.py
@@ -2,9 +2,9 @@ import graphlab as gl
 
 
 wd = "/Users/linahu/Documents/Developer/CareerBuilder/"
-n_recs = 50
+n_recs = 150
 
-def write_to_file(model,testusers,filename)
+def write_to_file(model,testusers,filename):
     with open(filename, "w") as outfile:
         outfile.write("UserId, JobIds\n")
         for userID in list(testusers):
@@ -19,10 +19,15 @@ users =gl.SFrame.read_csv(wd + "users.tsv",header=True, delimiter='\t')
 
 trainset =apps[apps['Split']=="Train"]
 testset=apps[apps['Split']=="Test"]
+
 testusers = set(testset['UserID'])
+#split test users based on WindowID
+testusers1 = set(testset[testset['WindowID']=='1']['UserID'])
+
+
 
 #simple recommender
 model1 = gl.recommender.create(apps,user_id = 'UserID',item_id='JobID')
 
 #write recommendations to file
-write_to_file(model,test_users,wd+"Basic CF.csv")
+write_to_file(model1,testusers1,wd+"Basic CF.csv")
